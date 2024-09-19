8501024943188dd485b04e13d01805a1e17666c8
imzura <zurangely_portillo@soy.sena.edu.co>
2024-09-19 16:42:55 -0500

    final

---
+++ counter/counterService.js
@@ -0,0 +1,10 @@
+import Counter from '../counter/CounterModel.js';
+
+export async function getNextSequenceValue(sequenceName) {
+    const sequenceDocument = await Counter.findOneAndUpdate(
+        { _id: sequenceName },
+        { $inc: { sequence_value: 1 } },
+        { new: true, upsert: true }
+    );
+    return sequenceDocument.sequence_value;
+}
